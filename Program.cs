7b22db8b4a1f9b32f957b47c11ba526ad3097982
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-09-26 23:43:08 -0300

    Adiciona atividade VelocMedia

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VelocMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Objetivo: Calcule a velocidade, a partir da distância (Δd, em metros) e do tempo (Δt, em segundos).
+
+            Console.Write("Digite a distância percorrida (m): ");
+            int deltaD = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o tempo gasto (s): ");
+            int deltaT = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nVelocidade média: {deltaD/deltaT} m/s");
+        }
+    }
+}
